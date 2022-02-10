e38b85862731493305a287d95ac92af7dc7cb7da
uotlaf <uotlaf@protonmail.com>
2022-02-10 13:10:29 -0300

    Add Componente Título

--- App.js
+++ App.js
@@ -6,39 +6,51 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
-
-import TelaPrincipal from '@screens/TelaPrincipal';
-import TelaTeste from '@screens/TelaTeste';
-
-const App: () => Node = () => {
-  
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName = 'Principal'>
-
-      <Stack.Screen 
-        name = 'Principal'
-        component = {TelaPrincipal}
-      />
-
-
-      <Stack.Screen 
-        name = 'Teste'
-        component = {TelaTeste}
-      />
-
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-
-};
-
-
-export default App;
+ import React from 'react';
+ import type {Node} from 'react';
+ import { NavigationContainer } from '@react-navigation/native';
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ 
+ const Stack = createNativeStackNavigator();
+ 
+ import TelaPrincipal from '@screens/TelaPrincipal';
+ import TelaTeste from '@screens/TelaTeste';
+ 
+ 
+ import TituloTeste from '@components/Titulo'
+ 
+ const App: () => Node = () => {
+ 
+   return (
+     <NavigationContainer>
+       <Stack.Navigator initialRouteName = 'Principal' 
+         screenOptions={{
+           headerTitle: (props) => <TituloTeste {...props}/>,
+           headerBackVisible: "no"
+         }}
+       > 
+ 
+       
+       <Stack.Screen 
+         name = 'Principal'
+         component = {TelaPrincipal}
+         options={{ title: "Tela Principal" }}/>
+ 
+       
+ 
+ 
+       <Stack.Screen 
+         name = 'Teste'
+         component = {TelaTeste}
+         options={{ title: "Tela Teste" }}
+       />
+ 
+       </Stack.Navigator>
+     </NavigationContainer>
+   );
+ 
+ };
+ 
+ 
+ export default App;
+ 
