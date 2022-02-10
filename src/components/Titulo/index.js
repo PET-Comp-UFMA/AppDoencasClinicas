4e8da15f2455ac42f8de02bc62e1b2f793ec4255
uotlaf <uotlaf@protonmail.com>
2022-02-10 18:38:07 -0300

    Usar Mulish Bold para a barra de título

--- src/components/Titulo/index.js
+++ src/components/Titulo/index.js
@@ -1,69 +1,70 @@
-import React from "react";
-import { Component } from "react/cjs/react.production.min";
-import {
-    View,
-    TouchableOpacity,
-    Text,
-    StyleSheet
-} from "react-native";
+import React from 'react';
+import {Component} from 'react/cjs/react.production.min';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 
-import { useNavigation, useRoute } from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 
-
-import Seta from "icons/left.svg";
+import Seta from 'icons/left.svg';
 
 class Titulo extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const { navigation } = this.props;
-        this.navigation = navigation;
+    const {navigation} = this.props;
+    this.navigation = navigation;
 
-        var titulo = props.children;
-        this.titulo = titulo.toUpperCase();
+    var titulo    = props.children;
+    this.titulo   = titulo.toUpperCase();
 
+    this.setState = {title: titulo};
+  }
 
-        this.setState = { title: titulo }
+  render() {
+    let backbutton;
+    // Verifica se pode voltar
+    if (this.navigation.canGoBack()) {
+      backbutton = (
+        <TouchableOpacity onPress={this.navigation.goBack}>
+          <Seta height={25} stroke={'grey'} />
+        </TouchableOpacity>
+      );
+    } else {
+      backbutton = (
+        <TouchableOpacity>
+          <Seta height={25} stroke={''} />
+        </TouchableOpacity>
+      );
     }
 
-    
-    render(){
-        let backbutton;
-        // Verifica se pode voltar
-        if (this.navigation.canGoBack()) {
-            backbutton=<TouchableOpacity onPress={this.navigation.goBack}>
-                <Seta height={25} stroke={'grey'} />
-            </TouchableOpacity>
-        } else {
-            backbutton=<TouchableOpacity>
-                <Seta height={25} stroke={''} />
-            </TouchableOpacity>
-        }
+    return (
+      <View
+        flex={1}
+        flexDirection  = "row"
+        justifyContent = "flex-start"
+        alignItems     = "center">
+        {backbutton}
+        <View
+          flex={0.85} // Por algum motivo o react tem uma borda no header
+        >
+          <Text
+            width={120}
+            style={{
+              color: 'black',
+              fontSize: 30,
+              textAlign: 'center',
+              fontFamily: 'Mulish-Bold',
+              textAlignVertical: 'center',
+            }}>
+            {this.titulo}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
 
-        return(
-            <View flex={1}
-                flexDirection='row' 
-                justifyContent='flex-start'
-                alignItems='center' 
-            >
-            
-                {backbutton}
-            <View flex={0.85} // Por algum motivo o react tem uma borda no header
-            >  
-                <Text width={120} style={{color: 'black', 
-                    fontSize:30, textAlign:'center', textAlignVertical:'center' 
-                    }}>{this.titulo}
-                </Text>
-            </View>
+export default function (props) {
+  const navigation = useNavigation();
 
-          </View>
-        );
-    }
-};
-
-
-export default function(props) {
-    const navigation = useNavigation();
-
-    return <Titulo {...props} navigation={navigation}/>;
+  return <Titulo {...props} navigation={navigation} />;
 }
