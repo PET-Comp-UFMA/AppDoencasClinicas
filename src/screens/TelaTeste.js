c04390e4e4622420a239d4160824bacb6c0187c9
uotlaf <uotlaf@protonmail.com>
2022-02-11 12:24:06 -0300

    Mudar a background pra branco

--- src/screens/TelaTeste.js
+++ src/screens/TelaTeste.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        backgroundColor: '#BDBDBD',
+        backgroundColor: 'white',
         flexDirection: 'column',
         justifyContent: 'space-between',
     },
