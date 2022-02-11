1189a79afe061ded27976664d1fde70dfa8bb1d7
uotlaf <uotlaf@protonmail.com>
2022-02-11 11:04:11 -0300

    Conserta o App.js pra deixar menos feio

--- App.js
+++ App.js
@@ -1,56 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react';
+import type Node from 'react';
+import { NavigationContainer }      from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+ 
 
- import React from 'react';
- import type {Node} from 'react';
- import { NavigationContainer } from '@react-navigation/native';
- import { createNativeStackNavigator } from '@react-navigation/native-stack';
- 
- const Stack = createNativeStackNavigator();
- 
- import TelaPrincipal from '@screens/TelaPrincipal';
- import TelaTeste from '@screens/TelaTeste';
- 
- 
- import TituloTeste from '@components/Titulo'
- 
- const App: () => Node = () => {
- 
-   return (
-     <NavigationContainer>
-       <Stack.Navigator initialRouteName = 'Principal' 
-         screenOptions={{
-           headerTitle: (props) => <TituloTeste {...props}/>,
-           headerBackVisible: "no"
-         }}
-       > 
- 
+// Telas
+import TelaPrincipal from '@screens/TelaPrincipal';
+import TelaTeste     from '@screens/TelaTeste';
+
+// Componentes
+import Titulo        from '@components/Titulo';
+
+
+const Stack = createNativeStackNavigator();
+
+const App: () => Node = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = 'Principal' 
+        screenOptions={{
+          headerTitle: (props) => <Titulo {...props}/>,
+          headerBackVisible: "no"
+        }}> 
        
-       <Stack.Screen 
-         name = 'Principal'
-         component = {TelaPrincipal}
-         options={{ title: "Tela Principal" }}/>
+        <Stack.Screen 
+          name      = 'Principal'
+          component = {TelaPrincipal}
+          options   = {{ title: "Tela Principal" }}/>
  
-       
+        <Stack.Screen 
+          name      = 'Teste'
+          component = {TelaTeste}
+          options   = {{ title: "Tela Teste" }}/>
  
+      </Stack.Navigator>
+    </NavigationContainer>
+  ); 
+};
  
-       <Stack.Screen 
-         name = 'Teste'
-         component = {TelaTeste}
-         options={{ title: "Tela Teste" }}
-       />
- 
-       </Stack.Navigator>
-     </NavigationContainer>
-   );
- 
- };
- 
- 
- export default App;
+export default App;
  
