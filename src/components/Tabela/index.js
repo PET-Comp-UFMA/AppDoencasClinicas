ea9690c3c4cb9121a40d10fd694a2746d4395e40
uotlaf <uotlaf@protonmail.com>
2022-03-10 13:13:22 -0300

    Componente tabela com react-native-paper

--- src/components/Tabela/index.js
+++ src/components/Tabela/index.js
@@ -5,52 +5,158 @@ import {
     Text,
     StyleSheet,
 } from 'react-native';
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 import { useTheme } from '@react-navigation/native';
+import { DataTable } from 'react-native-paper';
 
 
 class Tabela extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            tableHead: props.tableHead,
-            tableData: props.tableData
-        }
+        this.tableHead = [];
+        this.tableData = [];
     }
-    render(){
-        const state = this.state;
-        
-        let headRow;
-        let estiloDados;
-        if (state.tableHead){
-            headRow = <Row data={state.tableHead} style={styles.head}/>;
-        }
-        else {
-            headRow = <Row style={styles.head}  />;
-            estiloDados = null;
+    render() {
+
+        // Texto opcional
+        let Head;
+        if (this.props.tableHead) {
+            Head = (<DataTable.Header style={styles.border}>
+                        {this.tableHead}
+                    </DataTable.Header>)
         }
 
-        
-        const textstyle = StyleSheet.create({
-            data: {
-                fontSize: 12, 
-                margin: 6,
-                alignSelf: 'center',
-                color: this.props.colors.text,
-                fontFamily: "Mulish_Regular",
+        // Arrumar todos os títulos das colunas
+        for ( var numObjeto in this.props.tableHead ) {
+            this.tableHead.push(
+                <View
+                    key={Math.random().toString(36)} // Gera uma chave aleatória para cada objeto. Evita chaves iguais
+                    style={styles.tableHead}
+                >
+                    <Text style={styles.tableHeadText}>{this.props.tableHead[numObjeto].props.children}</Text>
+                </View>
+            );
+        }
+
+        // Configuração da fonte das células(para o tema noturno)
+        this.cellStyle = StyleSheet.create({
+            cell: {
+                        fontSize: 12,
+                        color: 'black',
+                        fontFamily: "Mulish_Regular",
+                        color : this.props.colors.text,
+                        textAlign: 'center',
+                        flex:1,
+                        textAlignVertical: 'center'
+                    }});
+
+        // Arrumar todas as colunas
+        for ( var numLinha in this.props.tableData) {
+            var dataLinha = [];
+
+            // Bota os objetos na linha
+            for( var numObjeto in this.props.tableData[numLinha] ) {
+
+                // Se o título está vazio, botar borda na primeira linha da data
+                if (!Head && numLinha == 0) {    
+                    if (numObjeto == 0) {
+                        dataLinha.push(
+                            <View flex={5} style={styles.tableDataFirstLeft}>
+                                <Text style={this.cellStyle.cell}>{this.props.tableData[numLinha][numObjeto].props.children}</Text>
+                            </View>
+                        )
+                        continue
+                    } else if (numObjeto == this.props.tableData[numLinha].length - 1) {
+                        dataLinha.push(
+                            <View flex={5} style={styles.tableDataFirstRight}>
+                                <Text style={this.cellStyle.cell}>{this.props.tableData[numLinha][numObjeto].props.children}</Text>
+                            </View>
+                        )
+                        continue
+                    }
+                }
+                
+                // ultima linha
+                if (numLinha == this.props.tableData.length - 1) {
+                    // Esquerda
+                    if (numObjeto == 0) { 
+                        dataLinha.push(
+                            <View flex={5} style={styles.tableDataLastLeft}>
+                                <Text style={this.cellStyle.cell}>{this.props.tableData[numLinha][numObjeto].props.children}</Text>
+                            </View>
+                        )
+                        continue
+                    // Direita
+                    } else if (numObjeto == this.props.tableData[numLinha].length - 1) {
+                        dataLinha.push(
+                            <View flex={5} style={styles.tableDataLastRight}>
+                                <Text style={this.cellStyle.cell}>{this.props.tableData[numLinha][numObjeto].props.children}</Text>
+                            </View>
+                        )
+                        continue
+                    // Centro e/ou qualquer outra
+                    } else {
+                        dataLinha.push(
+                            <View flex={5} style={styles.tableData}>
+                                <Text style={this.cellStyle.cell}>{this.props.tableData[numLinha][numObjeto].props.children}</Text>
+                            </View>
+                        )
+                        continue
+                    }
+                }
+                else {
+                    dataLinha.push(
+                        <View flex={5} style={styles.tableData}>
+                            <Text style={this.cellStyle.cell}>{this.props.tableData[numLinha][numObjeto].props.children}</Text>
+                        </View>
+                    )
+                }
             }
-        })
+
+            
+            // Bota a linha na lista
+            if (!Head && numLinha == 0) {
+                this.tableData.push(
+                    <DataTable.Row
+                        key={Math.random().toString(36)} // Gera uma chave aleatória para cada objeto. Evita chaves iguais
+                        style={styles.tableRowFirst}
+                    >
+                        {dataLinha}
+                    </DataTable.Row>
+                )
+            }
+            else if (numLinha == this.props.tableData.length - 1) {
+                this.tableData.push(
+                    <DataTable.Row
+                        key={Math.random().toString(36)} // Gera uma chave aleatória para cada objeto. Evita chaves iguais
+                        style={styles.tableRowLast}
+                    >
+                        {dataLinha}
+                    </DataTable.Row>
+                )
+            } else {
+                this.tableData.push(
+                    <DataTable.Row
+                        key={Math.random().toString(36)} // Gera uma chave aleatória para cada objeto. Evita chaves iguais
+                        style={styles.tableRow}
+                    >
+                        {dataLinha}
+                    </DataTable.Row>
+                )
+            }
+
+        }
 
         return (
-            <View style={styles.container}>
-                <Table borderStyle={styles.border} style={estiloDados}>
-                    {headRow}
-                    <Rows data={state.tableData} textStyle={textstyle.data}/>
-                </Table>
-            </View>
+          <View style={styles.container}>
+              {Head}
+              <DataTable>
+                {this.tableData}
+              </DataTable>
+          </View>  
         );
     }
 }
+
 
 export default function (props) {
     const { colors } = useTheme();
@@ -60,51 +166,72 @@ export default function (props) {
 }
 
 const styles = StyleSheet.create({
-    container: { 
-        flex: 1, 
-        padding: 16, 
-        paddingTop: 22.5, 
-        backgroundColor: 'white' // Lib não aplica modificações no texto. Veja https://github.com/Gil2015/react-native-table-component/issues/145
+    container: {
+        flex:1
     },
-    TableText: {
-        margin: 10
+    tableHead: {
+        flex:1,
     },
-    border: { 
-        borderWidth: 1, 
-        borderColor: '#59998D'
+    border: {
+        backgroundColor: '#59998D',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,  
+        marginHorizontal:16,
+        paddingHorizontal: 0
     },
-    head: { 
-        height: 30, 
-        backgroundColor: '#59998D', 
-        alignContent: 'center', 
-        borderTopStartRadius: 10, 
-        borderTopEndRadius: 10,
-        borderColor: '#FFF'
-        
-    },
-    data: {
-        borderTopRightRadius: 10, 
-        borderTopLeftRadius: 10,
-        borderTopStartRadius: 10,
-    },
-    textHead: { 
+    tableHeadText: {
         fontSize: 12, 
-        margin: 6,
         alignSelf: 'center',
         color: '#FFFFFF',
         fontFamily: "Mulish_Bold",
+        flex:1,
+        textAlignVertical:'center',
+        textAlign: 'center'
     },
-    textData: { 
-        fontSize: 12, 
-        margin: 6,
-        alignSelf: 'center',
-        color: 'black',
-        fontFamily: "Mulish_Regular",
+    tableData: {
+        borderWidth: 1, 
+        borderColor: '#59998D',
     },
-    text: { 
-        margin: 6 
+    tableDataFirstLeft: {
+        borderWidth: 1, 
+        borderColor: '#59998D',
+        borderTopLeftRadius: 10,
     },
-    vazio: {
-
+    tableDataFirstRight: {
+        borderWidth: 1, 
+        borderColor: '#59998D',
+        borderTopRightRadius: 10,
+    },
+    tableDataLastLeft: {
+        borderWidth: 1, 
+        borderColor: '#59998D',
+        borderBottomLeftRadius: 10    
+    },
+    tableDataLastRight: {
+        borderWidth: 1, 
+        borderColor: '#59998D',
+        borderBottomRightRadius: 10
+    },
+    tableRow: {
+        marginHorizontal:16,
+        paddingHorizontal:0,
+        borderWidth: 1, 
+        borderColor: '#59998D',
+    },
+    tableRowFirst: {
+        marginHorizontal:16,
+        paddingHorizontal:0,
+        borderWidth: 1, 
+        borderColor: '#59998D',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,  
+    },
+    tableRowLast: {
+        marginHorizontal:16,
+        paddingHorizontal:0,
+        borderWidth: 1, 
+        borderColor: '#59998D',
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,  
     }
-});
+})
