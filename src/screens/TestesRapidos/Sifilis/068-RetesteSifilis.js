5309bdf7c798e77cd941d0642af912d9beb10c6c
Matheus Levy <levybessa2001@hotmail.com>
2022-03-13 21:32:09 -0300

    tela 063-071

--- src/screens/TestesRapidos/Sifilis/068-RetesteSifilis.js
+++ src/screens/TestesRapidos/Sifilis/068-RetesteSifilis.js
@@ -9,8 +9,11 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useTheme } from '@react-navigation/native';
+import Parag from '@components/Parag';
+import Bold from '@components/Bold';
+import Botao from '@components/Botao';
 
-export default function TelaPrincipal({ navigation }) {
+export default function T068({ navigation }) {
         // Atualiza as cores se o modo noturno estiver ligado
         const { colors } = useTheme();
 
@@ -29,10 +32,25 @@ export default function TelaPrincipal({ navigation }) {
         <SafeAreaView style={styles.container}>
             <ScrollView>
                 <View >
+                    <Parag>Se seu paciente não concordou realizar o <Bold>RETESTE</Bold> após teste reagente, aconselhe-o. E se ele aceitar após aconselhamento, clique em <Bold>REALIZAR RETESTE</Bold>.</Parag>
+                    <Parag>Se não for esse o caso, clique em <Bold>FINALIZAR</Bold> e será direcionado ao <Bold>MENU PRINCIPAL</Bold>.</Parag>
+                </View>
+                <View style={styles.containerBotao}>
 
-                    <Text>Em desenvolvimento</Text>
+                    <Botao
+                        title="ACONSELHAMENTO"
+                        onPress={() => navigation.navigate('072-Aconselhamento')}
+                    />
+                    <Botao
+                        title="REALIZAR RETESTE"
+                        onPress={() => navigation.navigate('')}
+                    />
 
-
+                    <Botao
+                        title="FINALIZAR"
+                        onPress={() => navigation.navigate('')}
+                    />
+                    
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -45,12 +63,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        
+        justifyContent: 'center',
         flexDirection: 'column',
         justifyContent: 'space-between',
-    },
-    containerBotao: {
+        
+        paddingTop: 15,
         width: '100%'
     },
-    
+    containerBotao: {
+        marginTop: 30,
+        width: '100%'
+    },
+    texto: {
+        marginBottom: 10,
+        
+    },
 });
--- src/screens/TestesRapidos/Sifilis/069-TesteRapidoSifilis.js
+++ src/screens/TestesRapidos/Sifilis/069-TesteRapidoSifilis.js
@@ -9,8 +9,11 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useTheme } from '@react-navigation/native';
+import Parag from '@components/Parag';
+import Bold from '@components/Bold';
+import Botao from '@components/Botao';
 
-export default function TelaPrincipal({ navigation }) {
+export default function T069({ navigation }) {
         // Atualiza as cores se o modo noturno estiver ligado
         const { colors } = useTheme();
 
@@ -29,10 +32,20 @@ export default function TelaPrincipal({ navigation }) {
         <SafeAreaView style={styles.container}>
             <ScrollView>
                 <View >
+                    <Parag>Caso não considere realizar o <Bold>RETESTE</Bold>, e o paciente concorde em prosseguir, clique em <Bold>MANEJO CLÍNICO</Bold> para prosseguir e será direcionado para as possíveis soluções.</Parag>
+                    <Parag>Caso o paciente não concorde em prosseguir, aconselhe-o. Se ele concordar, clique em <Bold>MANEJO CLÍNICO</Bold>, se mesmo assim ele não concordar, não há mais nada a fazer além de sensibilizar, então, clique em <Bold>FINALIZAR</Bold> e será direcionado ao <Bold>MENU PRINCIPAL</Bold>. Registre tudo e notifique.</Parag>
+                </View>
+                <View style={styles.containerBotao}>
 
-                    <Text>Em desenvolvimento</Text>
-
-
+                    <Botao
+                        title="MANEJO CLÍNICO"
+                        onPress={() => navigation.navigate('072-Aconselhamento')}
+                    />
+                    <Botao
+                        title="FINALIZAR"
+                        onPress={() => navigation.navigate('')}
+                    />
+                    
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -45,12 +58,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        
+        justifyContent: 'center',
         flexDirection: 'column',
         justifyContent: 'space-between',
-    },
-    containerBotao: {
+        
+        paddingTop: 15,
         width: '100%'
     },
-    
+    containerBotao: {
+        marginTop: 30,
+        width: '100%'
+    },
+    texto: {
+        marginBottom: 10,
+        
+    },
 });
