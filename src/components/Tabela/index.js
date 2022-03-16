a65a96ee67b541102c440f05a5a3ffba385ebb1f
uotlaf <uotlaf@protonmail.com>
2022-03-16 12:54:17 -0300

    Passar onPress para célula na tabela

--- src/components/Tabela/index.js
+++ src/components/Tabela/index.js
@@ -104,7 +104,7 @@ class Tabela extends Component {
                                 style={style}
                                 key={Math.random().toString(36)}
                                 >
-                                <Text style={this.cellStyle.cell}>{this.props.tableData[numLinha][numObjeto].props.children}</Text>
+                                <Text style={this.cellStyle.cell} onPress={this.props.tableData[numLinha][numObjeto].props.onPress}>{this.props.tableData[numLinha][numObjeto].props.children}</Text>
                             </View>
                 )
             }
