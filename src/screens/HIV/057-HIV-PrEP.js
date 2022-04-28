4f0ef7d71f69b4849cd363a36eed9e1d6c61e8be
uotlaf <uotlaf@protonmail.com>
2022-04-28 13:06:03 -0300

    Tabela "057-HIV-PrEP"

--- src/screens/HIV/057-HIV-PrEP.js
+++ src/screens/HIV/057-HIV-PrEP.js
@@ -10,6 +10,7 @@ import {
 import Botao from '../../components/Botao';
 import Bold from '../../components/Bold';
 import { useTheme } from '@react-navigation/native';
+import { DataTable } from 'react-native-paper';
 
 export default function T057({ navigation }) {
     // Atualiza as cores se o modo noturno estiver ligado
@@ -25,12 +26,94 @@ export default function T057({ navigation }) {
         textAlign: 'center',
         lineHeight: 35
     }
+
+    var style = {
+        fontSize: 11,
+        margin: 6,
+        alignSelf: 'center',
+        color: colors.text,
+        fontFamily: "Mulish_Regular"
+    }
+
     return (
         <SafeAreaView style={styles.container}>
 
             <ScrollView>
                     
-                {/* TABELA AQUI */}
+                <View>
+                    <DataTable flex={1}>
+                        <DataTable.Header style={styles.header}>
+                            <Text style={styles.tableHeadText}>
+                                Segmentos Populacionais Prioritários
+                            </Text>
+                            <Text style={styles.tableHeadText}>
+                                Definição
+                            </Text>
+                            <Text style={styles.tableHeadText}>
+                                Critério de indicação de PREP
+                            </Text>
+                        </DataTable.Header>
+                        
+                        <DataTable.Row>
+
+                        <View flex={2}>
+                                <View flex={1} flexDirection={'row'}>
+                                        <View style={styles.tableData} flex={1}>
+                                            <Text style={style}><Bold>Gays e outros homens que fazem sexo com homens(HSH)</Bold></Text>
+                                        </View>
+                                        <View style={styles.tableData} flex={1}>
+                                            <Text style={style}>Homens que se relacionam sexualmente e/ou afetivamente com outros homens</Text>
+                                        </View>
+                                </View>
+                                <View flex={1} flexDirection={'row'}>
+                                        <View style={styles.tableData} flex={1}>
+                                            <Text style={style}><Bold>Pessoas Trans</Bold></Text>
+                                        </View>
+                                        <View style={styles.tableData} flex={1}>
+                                            <Text style={style}>Pessoas que expressam um gênero diferente do sexo definido ao nascimento. Nesta definição são incluídos: homens e mulheres transexuais, transgêneros, travestis e outras pessoas com gêneros não binários</Text>
+                                        </View>
+                                </View>
+                                <View flex={1} flexDirection={'row'}>
+                                        <View style={styles.tableData} flex={1}>
+                                            <Text style={style}><Bold>Profissionais do sexo</Bold></Text>
+                                        </View>
+                                        <View style={styles.tableData} flex={1}>
+                                            <Text style={style}>Homens, Mulheres e pessoas trans que recebem dinheiro ou benefícios em troca de serviços sexuais, regular ou ocasionalmente</Text>
+                                        </View>
+                                </View>
+                            </View>        
+                            <View style={styles.tableData} flex={1}>
+                                <Text style={style}>Relação sexual anal(receptiva ou insertiva) ou vaginal, sem uso de preservativo, nos últimos seis meses 
+                                {'\n\n'} 
+                                E/OU
+                                {'\n\n'}
+                                Episódios recorrentes de Infecções Sexualmente Transmissíveis(IST)
+                                {'\n\n'}
+                                E/OU
+                                {'\n\n'}
+                                Uso repetitivo de Profilaxia Pós-Exposição(PEP)</Text>
+                            </View>
+
+
+                        </DataTable.Row>
+                        <DataTable.Row>
+                            <View style={[styles.tableData, styles.borderBottomLeft]} flex={1}>
+                                <Text style={style}><Bold>Parceiras sorodiscordantes para o HIV</Bold></Text>
+                            </View>
+
+                            <View flex={2}>
+                                <View flex={1} flexDirection={'row'}>
+                                        <View style={styles.tableData} flex={1}>
+                                            <Text style={style}>Parceria Heterossexual ou homossexual na qual uma das pessoas é infectada pelo HIV e outra não</Text>
+                                        </View>
+                                        <View style={[styles.tableData, styles.borderBottomRight]} flex={1}>
+                                            <Text style={style}>Relação sexual anal ou vaginal com uma pessoa infectada pelo HIV sem preservativo</Text>
+                                        </View>
+                                </View>
+                            </View>
+                        </DataTable.Row>
+                    </DataTable>
+                </View>
                 
                 <Text>
                     Indica-se para a PrEP a combinação de tenofovir associado a entricitabina, em dose fixa combinada TDF/FTC 300/200mg, um comprimido por dia, via oral, em uso contínuo.{"\n"}{"\n"}
@@ -68,24 +151,64 @@ const styles = StyleSheet.create({
     },
 
     tabela: {
-        marginTop: -5,
-        marginBottom: 50
+        marginTop: -20
     },
-    
-    textTitle: { 
-        fontSize: 18, 
+
+    textData: {
+        fontSize: 16,
         margin: 6,
-        marginLeft: 20,
-        textAlign: "left",
+        textAlign: 'left',
+        color: 'black',
         fontFamily: "Mulish_Regular",
     },
 
-    texto: {
-        marginBottom: 10,
-
+    textHead: {
+        color: "#fff",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 18
     },
-
-    containerBotao: {
-        marginTop       :  30,
+    tableCell: {
+        flex: 1
+    },
+    header: {
+        backgroundColor: '#59998D',
+        marginHorizontal:16,
+        paddingHorizontal: 0,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        borderBottomColor: "#59998D"
+    },
+    tableHeadText: {
+        fontSize: 12, 
+        alignSelf: 'center',
+        color: '#FFFFFF',
+        fontFamily: "Mulish_Bold",
+        flex:1,
+        textAlignVertical:'center',
+        textAlign: 'center'
+    },
+    tableData: {
+        borderWidth: 1, 
+        borderColor: '#59998D',
+        
+    },
+    tableRow: {
+        marginHorizontal:16,
+        paddingHorizontal:0,
+        borderWidth: 1, 
+        borderColor: '#59998D',
+    },
+    borderTopLeft: {
+        borderTopLeftRadius: 10
+    },
+    borderTopRight: {
+        borderTopRightRadius: 10
+    },
+    borderBottomLeft: {
+        borderBottomLeftRadius: 10
+    },
+    borderBottomRight: {
+        borderBottomRightRadius: 10
     }
 })
